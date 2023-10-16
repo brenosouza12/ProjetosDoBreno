b3dd9b47c38394b48532ec753b21b5047fb68c26
brenosouza12 <breno_h_souza@estudante.sesisenai.org.br>
2023-10-16 14:58:40 -0300

    Mais um atualizada no app de onibus

--- 11-out-23-Onibus-Aspectverso/App.js
+++ 11-out-23-Onibus-Aspectverso/App.js
@@ -4,13 +4,53 @@ import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-nativ
 export default function App() {
 
   const [buses, setBuses] = useState([]);
-  const [busInfo, setBusInfo] = useState({ empresa: '', prefixo: '', chassi: '', ano: '', placa: '', modelo: '', });
+  const [busInfo, setBusInfo] = useState({
 
+    empresa: '',
+    prefixo: '',
+    chassi: '',
+    ano: '',
+    placa: '',
+    modelo: '',
+
+  });
+
+  const [editingBus, setEditingBus] = useState(null);
 
   const addBus = () => {
 
-    setBuses([...buses, busInfo]);
+    if (editingBus !== null) {
+
+      const updatedBuses = buses.map((bus, index) =>
+        index === editingBus ? busInfo : bus
+
+      );
+
+      setBuses(updatedBuses);
+      setEditingBus(null);
+
+    } else {
+
+      setBuses([...buses, busInfo]);
+
+    }
+
     setBusInfo({ empresa: '', prefixo: '', chassi: '', ano: '', placa: '', modelo: '' });
+
+  };
+
+  const editBus = (index) => {
+
+    const busToEdit = buses[index];
+    setBusInfo({ ...busToEdit });
+    setEditingBus(index);
+
+  };
+
+  const deleteBus = (index) => {
+
+    const updatedBuses = buses.filter((_, i) => i !== index);
+    setBuses(updatedBuses);
 
   };
 
@@ -21,29 +61,29 @@ export default function App() {
       <Text style = {styles.header}>Onibus Aspectverso</Text>
 
       <TextInput
-
+      
         style = {styles.input}
         placeholder = "Empresa"
         value = {busInfo.empresa}
-        onChangeText = {text => setBusInfo({ ...busInfo, empresa: text })}
-
-      />
-
-      <TextInput 
-
-        style = {styles.input}
-        placeholder = "Prefixo"
-        value = {busInfo.prefixo}
-        onChangeText = {text => setBusInfo({ ...busInfo, prefixo: text })}
+        onChangeText = {(text) => setBusInfo({ ...busInfo, empresa: text })}
 
       />
 
       <TextInput
-      
+
+        style = {styles.input}
+        placeholder = "Prefixo"
+        value = {busInfo.prefixo}
+        onChangeText = {(text) => setBusInfo({ ...busInfo, prefixo: text })}
+
+      />
+
+       <TextInput
+
         style = {styles.input}
         placeholder = "Modelo"
         value = {busInfo.modelo}
-        onChangeText = {text => setBusInfo({ ...busInfo, modelo: text })}
+        onChangeText = {(text) => setBusInfo({ ...busInfo, modelo: text })}
 
       />
 
@@ -52,7 +92,7 @@ export default function App() {
         style = {styles.input}
         placeholder = "Chassi"
         value = {busInfo.chassi}
-        onChangeText = {text => setBusInfo({ ...busInfo, chassi: text })}
+        onChangeText = {(text) => setBusInfo({ ...busInfo, chassi: text })}
 
       />
 
@@ -61,7 +101,7 @@ export default function App() {
         style = {styles.input}
         placeholder = "Ano"
         value = {busInfo.ano}
-        onChangeText = {text => setBusInfo({ ...busInfo, ano: text })}
+        onChangeText = {(text) => setBusInfo({ ...busInfo, ano: text })}
 
       />
 
@@ -70,20 +110,19 @@ export default function App() {
         style = {styles.input}
         placeholder = "Placa"
         value = {busInfo.placa}
-        onChangeText = {text => setBusInfo({ ...busInfo, placa: text })}
+        onChangeText = {(text) => setBusInfo({ ...busInfo, placa: text })}
 
       />
 
-      <Button title = "Adicionar Ônibus" onPress = {addBus} />
+      <Button title = {editingBus !== null ? 'Editar Ônibus' : 'Adicionar Ônibus'} onPress = {addBus} />
 
       <FlatList
 
         data = {buses}
         keyExtractor = {(item, index) => index.toString()}
-        renderItem = {({ item }) => (
+        renderItem = {({ item, index }) => (
 
           <View style = {styles.busItem}>
-
             <Text style = {styles.busItemText}>Empresa: {item.empresa}</Text>
             <Text style = {styles.busItemText}>Prefixo: {item.prefixo}</Text>
             <Text style = {styles.busItemText}>Modelo: {item.modelo}</Text>
@@ -91,6 +130,12 @@ export default function App() {
             <Text style = {styles.busItemText}>Ano: {item.ano}</Text>
             <Text style = {styles.busItemText}>Placa: {item.placa}</Text>
             
+            <View style = {styles.buttonContainer}>
+
+              <Button title = "Editar" onPress = {() => editBus(index)} />
+              <Button title = "Excluir" onPress = {() => deleteBus(index)} />
+
+            </View>
 
           </View>
 
@@ -110,15 +155,16 @@ const styles = StyleSheet.create({
 
     flex: 1,
     padding: 16,
-    backgroundColor: '#f4f4f4',
+    backgroundColor: '#f0f0f0',
 
   },
 
   header: {
 
-    fontSize: 28,
-    marginBottom: 16,
+    fontSize: 24,
+    marginBottom: 10,
     textAlign: 'center',
+    fontWeight: 'bold',
     color: '#333',
 
   },
@@ -130,27 +176,46 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 10,
     padding: 8,
-    borderRadius: 4,
     backgroundColor: '#fff',
 
   },
 
   busItem: {
-
     marginVertical: 10,
     padding: 10,
     backgroundColor: '#fff',
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: '#ccc',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
 
   },
 
   busItemText: {
 
     fontSize: 16,
-    marginBottom: 4,
+    marginBottom: 5,
+    color: '#333',
 
   },
 
+  buttonContainer: { 
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+  },
+
+  button: {
+
+    flex: 1,
+    margin: 5,
+
+  },
+  
 });
